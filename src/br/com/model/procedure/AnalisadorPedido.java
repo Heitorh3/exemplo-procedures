fc00514a74f4dc850c57d74e14d65d68f3c39630
Heitor Neto <heitorh3@gmail.com>
2016-12-20 14:11:47 -0200

    Teste na procedure

--- src/br/com/model/procedure/AnalisadorPedido.java
+++ src/br/com/model/procedure/AnalisadorPedido.java
@@ -1,10 +1,29 @@
 package br.com.model.procedure;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.ParameterMode;
+import javax.persistence.Persistence;
+import javax.persistence.StoredProcedureQuery;
+
 public class AnalisadorPedido {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("exemploProceduresPU");
+		EntityManager manager = factory.createEntityManager();
+		
+		StoredProcedureQuery storedProcedure = manager.createStoredProcedureQuery("analisarPedido");
+		storedProcedure.registerStoredProcedureParameter("codigoPedido", Long.class, ParameterMode.IN);
+		storedProcedure.setParameter("codigoPedido", 2L);
+		storedProcedure.registerStoredProcedureParameter("analise", String.class, ParameterMode.OUT);
+		
+		storedProcedure.execute();
+		
+		String analise = (String) storedProcedure.getOutputParameterValue("analise");
+		System.out.println("A análise foi: " + analise);
+
+		
 	}
 
 }
